a3938932f742deab93f5f6ae63aa72b089335a07
ZHANG YUJIA <zhyjmooc@gmail.com>
2023-10-06 16:22:57 +0300

    Step1-5 Done

---
+++ PrinterApp.java
@@ -0,0 +1,15 @@
+public class PrinterApp {
+
+	public static void main(String[] args) {
+		// PrinterApp Exercise
+		// Step 2
+		System.out.println("Printer App");
+		
+		// Step 3 
+		System.out.println("This app is made by : ");
+		
+		// Step 4 : Declare a string typed variable named "creator".
+		String creator = "YUJIA";
+		
+		// Step 5 : Print the value of the creator to the console.
+		System.out.println("This app is made by : " + creator);
